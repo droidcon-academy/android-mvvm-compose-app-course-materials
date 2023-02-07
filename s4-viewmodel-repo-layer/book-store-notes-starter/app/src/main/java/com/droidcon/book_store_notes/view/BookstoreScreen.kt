f3b3be9ba55a5b210e0b201e2f57c915f8346f9e
cata <catalinstefan@Catalins-MacBook-Pro.local>
2023-02-07 10:53:39 +0700

    Add section 3 content

---
+++ s4-viewmodel-repo-layer/book-store-notes-starter/app/src/main/java/com/droidcon/book_store_notes/view/BookstoreScreen.kt
@@ -0,0 +1,9 @@
+package com.droidcon.book_store_notes.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BookstoreScreen() {
+    Text(text = "Bookstore screen")
+}
