c6a0b1b07e3c74e69b2498b39c5ceaae88bb73e2
cata <catalinstefan@Catalins-MacBook-Pro.local>
2023-02-08 10:57:45 +0700

    Update s1

---
+++ s1-getting-started/book-store-notes-final/app/src/main/java/com/droidcon/book_store_notes/BookstoreNotesApplication.kt
@@ -0,0 +1,6 @@
+package com.droidcon.book_store_notes
+
+import android.app.Application
+
+class BookstoreNotesApplication: Application() {
+}
--- s1-getting-started/book-store-notes-final/app/src/main/java/com/droidcon/bookstorenotes/BookstoreNotesApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.droidcon.bookstorenotes
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BookstoreNotesApplication: Application() {
-}
